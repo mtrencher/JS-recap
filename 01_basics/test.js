aa2088e5a3572133618de06827f722b85419d9e5
mtrencher <ishan@mtrench.com>
2025-01-26 09:50:23 +0000

    container config done and test file is running

---
+++ 01_basics/test.js
@@ -0,0 +1,4 @@
+console.log("Welcome everyone! this is the test file");
+
+// since the file is in a folder we have to entered in the folder by comand "cd foldername"
+// After that use node filename to execute the file
