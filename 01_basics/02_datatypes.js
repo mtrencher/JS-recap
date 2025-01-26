522bdd921f34fddddda1f272d14888c7e2f7acf5
mtrencher <ishan@mtrench.com>
2025-01-26 11:00:20 +0000

    basic of datatype done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+let id = "mtrencher" // string
+let age = 23 // number
+let isLoggiedIn = true // boolean
+let noNumber = NaN // not a number //type => undefined
+let a = undefined // undefined // type => undefined
+let b = null // standalone value also a data type // type => object
+let s = Symbol // uses for uniquiness // type => function 
+
+console.table([id,age, isLoggiedIn, noNumber, a, b, s])
+// these are the primitive data-types
+
+//console.log(typeof a);
+
+
+// there are other data types as well but as a begunder its good to know but, we are not going to use it righ now
+// eg: bigint, symbol 
+
+// task =>
+    // check the type of all the datatype showing in the upwords.
+// hint => use "typeof" 🤫
