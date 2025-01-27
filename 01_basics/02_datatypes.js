4d1af54957a8a717bfd2f808c3dad3b19bfa7761
mtrencher <ishan@mtrench.com>
2025-01-27 13:37:38 +0000

    Other theory on data-types

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -18,3 +18,12 @@ console.table([id,age, isLoggiedIn, noNumber, a, b, s])
 // task =>
     // check the type of all the datatype showing in the upwords.
 // hint => use "typeof" 🤫
+
+// ====================Theory====================
+// There are mainly 2 type of data-type => 1. Primitive, 2. Non-Primitive (reference type)
+// the data type are divivded base on call by value and call by reference.
+// In the call by value the copy of data is given to the user not the actual one.
+// In the call by reference, the memory address is given to the user.
+
+// there are 7 primitive data-type => String, Boolean, null, undefined, BigInt, Symbol, Number
+// Non-primitive => array, object, function.
